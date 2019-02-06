b243a2dd9d38fb06b64643dd48f1f552936af3f4
Aswin Karthik <aswinkarthik93@gmail.com>
2019-02-06 18:47:04 +0530

    Abstract installing all hashistack tools using one script

---
+++ scripts/hashistack.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+set -e
+
+function install_tool {
+    TOOL=$1
+    VERSION=$2
+
+    echo "Installing ${TOOL}-${VERSION}..."
+
+    cd /tmp/
+    curl -sSL https://releases.hashicorp.com/${TOOL}/${VERSION}/${TOOL}_${VERSION}_linux_amd64.zip -o ${TOOL}.zip
+    unzip ${TOOL}.zip
+    sudo chmod +x ${TOOL}
+    sudo mv ${TOOL} /usr/bin/${TOOL}
+    sudo mkdir -p /etc/${TOOL}.d
+    sudo chmod a+w /etc/${TOOL}.d
+    sudo rm -rf /tmp/${TOOL} /tmp/${TOOL}.zip
+    echo "Installation of ${TOOL}-${VERSION} is complete"
+}
+
+install_tool "nomad" "0.8.7"
+install_tool "consul" "1.4.0"
+install_tool "vault" "1.0.2"
+
+echo "Installing http-echo"
+curl -sSL https://github.com/hashicorp/http-echo/releases/download/v0.2.3/http-echo_0.2.3_linux_amd64.zip -o http-echo.zip
+unzip http-echo.zip
+sudo mv http-echo /usr/local/bin
