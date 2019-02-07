5b90e4f2db6236293f924f8473ed74c4c305d64c
Aswin Karthik <aswinkarthik93@gmail.com>
2019-02-07 19:35:11 +0530

    Install consul-template, set NOMAD_ADDR port and install dnsmasq

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -3,4 +3,8 @@ echo "Installing required packages..."
 sudo apt-get update
 
 # Other Packages required
-sudo apt-get install unzip curl vim jq dnsutils -y
+sudo apt-get install unzip curl vim jq dnsutils dnsmasq -y
+
+echo "server=/consul/127.0.0.1#8600" > /etc/dnsmasq.d/10-consul
+
+sudo systemctl restart dnsmasq
--- scripts/hashistack.sh
+++ scripts/hashistack.sh
@@ -23,8 +23,11 @@ function install_tool {
 install_tool "nomad" "0.8.7"
 install_tool "consul" "1.4.0"
 install_tool "vault" "1.0.2"
+install_tool "consul-template" "0.19.5"
 
 echo "Installing http-echo"
 curl -sSL https://github.com/hashicorp/http-echo/releases/download/v0.2.3/http-echo_0.2.3_linux_amd64.zip -o http-echo.zip
 unzip http-echo.zip
 sudo mv http-echo /usr/local/bin
+
+echo "export NOMAD_ADDR=http://192.168.1.100:4646" > /etc/profile.d/nomad-cli.sh
